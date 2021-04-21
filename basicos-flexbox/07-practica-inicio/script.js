ea41a6c5c5b09e7dd06328565d84494a9b21c288
malpica2803 <simbron_malpica@hotmail.com>
2021-04-21 07:27:47 -0500

    Una serie de archivos y notas importantes

---
+++ basicos-flexbox/07-practica-inicio/script.js
@@ -0,0 +1,31 @@
+"use strict";
+const masaAdol = Number("81");
+const altAdol = 1.75;
+const masaBecc = 86;
+const altBecc = 175;
+
+if (masaAdol > 80) {
+  console.log(`Usted esta pasado de Peso por ${masaAdol - 80} Kilos
+  y tiene una altura de ${altAdol} mts `);
+} else {
+  console.log(`Usted  esta esn su Peso Ideal`);
+}
+
+console.log("5", 5);
+console.log(String(56));
+
+let mens = console.log(1 + 5 + 6 + "5");
+typeof mens;
+console.log(14 - 4 - 3 - 5 - 1 + "1");
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Adolfo"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const aEntrada = "1991";
+console.log(Number(aEntrada));
+console.log(aEntrada + 18);
+
+console.log(Number("adolfo"));
